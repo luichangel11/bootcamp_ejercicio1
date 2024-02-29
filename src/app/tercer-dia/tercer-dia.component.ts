d718020d962f436e4af32a49259c78f3a82b548e
luichangel11 <luichtec@gmail.com>
2024-02-29 12:24:20 -0700

    añadiendo el componente del tercer dia

--- src/app/tercer-dia/tercer-dia.component.ts
+++ src/app/tercer-dia/tercer-dia.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { ExampleService } from '../example.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tercer-dia',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './tercer-dia.component.html',
   styleUrl: './tercer-dia.component.sass'
 })
 export class TercerDiaComponent {
-
+  name = '';
+  user: any = [];
+  constructor(public service: ExampleService){}
+  ngOnInit() {
+  this.service.obtenerLista().subscribe( resp =>{
+    console.log(resp);
+    this.user = resp;
+  })
+  }
+  editarTexto(text:string){
+    console.log(text)
+    return text;
+  }
 }
